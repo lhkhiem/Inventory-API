7798556126b5bf8d1dcd692d61037ae95e317715
Khiem Le <hoangkhiemkg@outlook.com.vn>
2021-11-18 22:10:30 +0700

    Tạo project data

---
+++ Data/EF/DbContextFactory.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace Invetory.Data.EF
+{
+    public class DbContextFactory : IDesignTimeDbContextFactory<DbContext>
+    {
+        public DbContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var connectionString = configuration.GetConnectionString("InventoryDb");
+            var optionsBuilder = new DbContextOptionsBuilder<EShopDbContext>();
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new EShopDbContext(optionsBuilder.Options);
+        }
+    }
+}
